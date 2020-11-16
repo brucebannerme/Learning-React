636a4eed5a27803ac5d2d731c946d48eb8b75d79
Ranveer Sequeira <ranveersequeira@gmail.com>
2020-11-16 13:22:20 +0530

    added my pets

---
+++ src/app.js
@@ -0,0 +1,30 @@
+
+const Pet = (props) => {
+    return React.createElement("div",{} , [
+        React.createElement("h1",{}, props.name),
+        React.createElement("h2",{}, props.animal),
+        React.createElement("h2",{}, props.breed),
+        
+    ]);
+    
+};
+
+
+const App = () => {
+
+    return React.createElement(
+        "div",
+        {},
+       [ React.createElement("h1",{}, "Adopt Me"),
+         React.createElement(Pet,{name: "Sheer", animal: "lion" , breed: "idk"}),
+         React.createElement(Pet,{name: "Bagh", animal: "bagh" , breed: "idk"}),
+         React.createElement(Pet,{name: "Cheetah", animal: "cheetah" , breed: "idk"}),
+
+         
+    ]
+    );
+
+};
+
+ReactDOM.render(React.createElement(App),
+document.getElementById("root"))
